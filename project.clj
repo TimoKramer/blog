a08b862bcf7c729de72ff2114148a5f545fc9f6f
Timo Kramer <fw-git@timokramer.de>
2020-02-13 10:54:00 +0100

    updated deps

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
             :url "https://github.com/lacarmen/cryogen"
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.10.0"]
-                           [ring/ring-devel "1.7.1"]
+            :dependencies [[org.clojure/clojure "1.10.1"]
+                           [ring/ring-devel "1.8.0"]
                            [compojure "1.6.1"]
                            [ring-server "0.5.0"]
                            [cryogen-markdown "0.1.11"]
-                           [cryogen-core "0.2.1"]]
+                           [cryogen-core "0.3.1"]]
             :plugins [[lein-ring "0.12.5"]]
             :main cryogen.core
             :ring {:init cryogen.server/init
